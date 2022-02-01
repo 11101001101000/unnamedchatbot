42001564cab0db068fda58afe53e69bb3f13de97
11101001101000 <t-h-0928@naver.com>
2022-02-02 00:46:49 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+feeling = 0
+kindness = 0
+print ('Hi! I am pacbot, Python Ai ChatBOT.')
+print ('Say anything to start conversation')
+usrmsg = input ('Say something: ')
+if str('안녕' in usrmsg) == 'True':
+    category = 'greeting'
+    kindness = kindness + 1
+if category == 'greeting':
+    feeling = feeling + kindness
+    if feeling > 0:
+        botmsg = '안녕!'
+print (botmsg)

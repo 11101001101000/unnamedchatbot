d6e030e41167a1dd8a26ad670c548939cf2c4e67
11101001101000 <t-h-0928@naver.com>
2022-02-08 17:03:03 +0900

    add message

--- bot_kr_dev.py
+++ bot_kr_dev.py
@@ -12,7 +12,15 @@ while end == 0:
         print ('안녕! 나는 이름없는 챗봇이야.')
         print ('대화를 시작하려면 아무 거나 말해 봐. 아, 들을 수는 없으니까 아무 말이나 써 봐.')
     usrmsg = input ('Say something: ')
-    if str ('' in usrmsg) == 'True':
-        kindness = kindness
-        feeling = feeling + kindness
-        print ('')
+    # if str ('' in usrmsg) == 'True':
+    #     kindness = kindness
+    #     feeling = feeling + kindness
+    #     print ('')
+    if str ('안녕' in usrmsg) == 'True':
+        if str ('안녕!' in usrmsg) == 'True':
+            kindness = kindness + 3
+            print ('안녕!!!')
+        else:
+            kindness = kindness + 1
+            feeling = feeling + kindness
+            print ('안녕!')

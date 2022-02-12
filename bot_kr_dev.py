d2489455a4b5abd44e1d41faf3839ae1ea19c438
11101001101000 <t-h-0928@naver.com>
2022-02-12 23:44:24 +0900

    add exit messages

--- bot_kr_dev.py
+++ bot_kr_dev.py
@@ -11,11 +11,15 @@ while end == 0:
         # greeting
         print ('안녕! 나는 이름없는 챗봇이야.')
         print ('대화를 시작하려면 아무 거나 말해 봐. 아, 들을 수는 없으니까 아무 말이나 써 봐.')
+        fchat = 1
     usrmsg = input ('Say something: ')
+    # Use the code below to add message(s)
     # if str ('' in usrmsg) == 'True':
     #     kindness = kindness
     #     feeling = feeling + kindness
     #     print ('')
+    #
+    # message: greeting
     if str ('안녕' in usrmsg) == 'True':
         if str ('안녕!' in usrmsg) == 'True':
             kindness = kindness + 3
@@ -27,6 +31,15 @@ while end == 0:
                 print ('안녕!')
             else:
                 print ('어 그래')
+    # message: quit
     if str ('가야' in usrmsg and '돼' in usrmsg) == 'True':
         if feeling > 0:
             quit = input ('진짜 가야 돼? ')
+            if str ('어' in quit or '응' in quit or '맞아' in quit) == 'True':
+                print ('대화 재밌었어. 잘 가!')
+                end = 1
+                exit()
+        else:
+            print ('그래')
+            end = 1
+            exit()
